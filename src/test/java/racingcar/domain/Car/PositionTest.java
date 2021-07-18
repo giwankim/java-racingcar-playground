e52fd096d4e89ab3f704160cbd7e25c6c826ccbc
Giwan Kim <giwankim@gmail.com>
2021-07-18 14:22:49 +0900

    feat(racingcar): Add Position
    Position is a wrapper class for valid position value

---
+++ src/test/java/racingcar/domain/Car/PositionTest.java
@@ -0,0 +1,20 @@
+package racingcar.domain.Car;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class PositionTest {
+
+    @Test
+    void constructor() {
+        Position position = new Position(4);
+        assertThat(position).isEqualTo(new Position(4));
+    }
+
+    @Test
+    void negative() {
+        assertThatThrownBy(() -> new Position(-1)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
