64b1a7620d6f615d8ba4b91da07f4d21ca74d5e1
Giwan Kim <giwankim@gmail.com>
2021-07-18 10:15:16 +0900

    style(racingcar): CarTest names are made more concise
    Test names are more concise and changed sourceCompatibility in build.gradle to 11

--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -1,38 +1,37 @@
-package racingcar.domain;
+package racingcar.domain.car;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.domain.car.Car;
 
 public class CarTest {
 
   private Car car;
-  private String name;
 
   @BeforeEach
   void setUp() {
-    name = "gwen";
-    car = new Car(name);
+    car = new Car("gwen");
   }
 
   @Test
-  @DisplayName("생성자가 작동한다")
-  void name() {
-    assertThat(car.getName()).isEqualTo(name);
+  @DisplayName("생성자")
+  void constructor() {
+    assertThat(car.getName()).isEqualTo(new Name("gwen"));
     assertThat(car.getPosition()).isEqualTo(0);
   }
 
   @Test
-  @DisplayName("자동차가 움직인다")
+  @DisplayName("이동")
   void move() {
     car.move(4);
     assertThat(car.getPosition()).isEqualTo(1);
   }
 
   @Test
-  @DisplayName("자동차가 움직이지 않는다")
+  @DisplayName("정지")
   void not_move() {
     car.move(3);
     assertThat(car.getPosition()).isEqualTo(0);
